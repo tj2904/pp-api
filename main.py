bc0097f1a5d57af2f511573552443d43ea182438
tj2904 <tj2904+github@gmail.com>
2023-05-04 16:11:44 +0100

    removes sentry test endpoint

--- main.py
+++ main.py
@@ -304,8 +304,3 @@ async def get_all_vader_scored_news_from_database():
     res = dbBasicVader.fetch([{"vaderSummary.compound?gte": 0.5}, {
         "vaderTitle.compound?gte": 0.5}])
     return {"data": res} if res else ({"message": "No news found"})
-
-
-@app.get("/sentry-debug", tags=["Utilities"])
-async def trigger_error():
-    division_by_zero = 1 / 0
